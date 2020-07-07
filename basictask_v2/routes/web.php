d262c81ed7b8d8ae387f7c20e3ac200cbbe6503f
baochanh1801 <duong.bao.chanh@sun-asterisk.com>
2020-07-07 16:04:25 +0700

    them task va delete task

--- basictask_v2/routes/web.php
+++ basictask_v2/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Requests\NameRequest;
+use App\Task;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,20 +16,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('tasks');
+    $tasks = Task::orderBy('created_at','asc')->get();
+    return view('tasks', ['tasks' => $tasks]);
 });
 
 
 /**
  * Add New Task
  */
-Route::post('/task', function (Request $request) {
-    //
+Route::post('/task', function (NameRequest   $request) {
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
 });
 
 /**
  * Delete Task
  */
 Route::delete('/task/{task}', function (Task $task) {
-    //
+    $task->delete();
+    return redirect("/");
 });
